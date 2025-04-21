3b9bdc6a0dcd7ce04d8d19475b2c12342d589da4
Jake Mwangi-Powell <psyjm13@exmail.nottingham.ac.uk>
2025-04-21 18:33:19 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Wait until the DOM is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    // Find the div with id="greeting"
+    const greetEl = document.getElementById('greeting');
+    // Set its text content
+    greetEl.textContent = 'Hello';
+  });
+  
